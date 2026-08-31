826efd3e2817f0a8e0c42b7c33729c446f6888d6
DINESH JENA <dineshakhay736@gmail.com>
2026-08-31 20:03:21 +0530

    frontend changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      Welcome to our gokuldhma society
+      Welcome to our gokuldhma society. main aatmaram tukaram bhide gokuldham society ka ekmevay secratory
     </div>
   )
 }
